01f956257a4466543631e40c246c8d45db6bd7c1
Norsi <natalieannorsi@gmail.com>
2016-11-30 22:57:09 -0800

    code cleanup for project submission

--- Programming-Foundations/MiniProjects/movie-website/media.py
+++ Programming-Foundations/MiniProjects/movie-website/media.py
@@ -1,22 +1,21 @@
 import webbrowser
 
 class Video():
+	"""This class provides a way to save video information."""
+	#initialize instance of Video object
 	def __init__(self, title, duration):
 		self.title = title
 		self.duration = duration
 
 class Movie(Video):
 	"""This class provides a way to save movie information."""
-	VALID_RATINGS = ['G','R','PG','PG-13']
-	#Refer to Google Python Style Guide
-	def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
+	VALID_RATINGS = ['G','R','PG','PG-13'] #valid possible ratings
+	def __init__(self, title, storyline, poster_image_url,
+				trailer_youtube_url):
 		self.title = title
 		self.storyline = storyline
 		self.poster_image_url = poster_image_url
 		self.trailer_youtube_url = trailer_youtube_url
 
 	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
-
-class TvShow(Video):
-	# would be the TV Show class
+		webbrowser.open(self.trailer_youtube_url) #open trailer url
