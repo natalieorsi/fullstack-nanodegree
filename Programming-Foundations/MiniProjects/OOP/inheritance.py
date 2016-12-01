29a595d3ee3ac937c21ee12bd97da6a9ba7b6e22
Norsi <natalieannorsi@gmail.com>
2016-11-30 17:15:18 -0800

    typo fix
    should probably define __init__, not just call it, der

--- Programming-Foundations/MiniProjects/OOP/inheritance.py
+++ Programming-Foundations/MiniProjects/OOP/inheritance.py
@@ -1,5 +1,6 @@
 def Parent():
-	__init__(self, last_name, eye_color):
+	def __init__(self, last_name, eye_color):
 		print("Parent constructor called")
 		self.last_name = last_name
 		self.eye_color = eye_color
+
