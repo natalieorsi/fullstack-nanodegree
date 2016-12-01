1e4092d008e369fe68f138f05fc36dd6fc3a6ebd
Norsi <natalieannorsi@gmail.com>
2016-12-01 15:32:00 -0800

    chose 6 random movies from the database
    also, continue to pull until 6 valid movie IDs are found

--- Programming-Foundations/movie-website/tmdb_api.py
+++ Programming-Foundations/movie-website/tmdb_api.py
@@ -3,51 +3,36 @@
 Created on Wed Nov 30 23:02:00 2016
 
 @author: norsi
+
+This code will not run without an API key from The Movie DB!
 """
 import tmdbsimple as tmdb
 import random
-#be sure to download the tmdb lib using pip install tmdbsimple
-
-#scifi = "http://www.themoviedb.com/discover/movie?with_genres=878"
-#connection = urllib.urlopen(scifi)
-#print connection.read()
-#connection.close()
-#print connection
+#be sure to download the tmdbsimple lib using pip install tmdbsimple
 
 youtube = 'http://www.youtube.com/watch?v='
 img_url = 'http://image.tmdb.org/t/p/w500'
-titles = {}
-videos = {}
-images = {}
+
 movies = {}
-attempt = 0 #safeguard against infinite loops
-#while (len(movies) < 2) and (attempt < 3):
-#    num = random.randint(1,1000)
-#    try:
-#        movie = tmdb.Movies(num).info()
-#        print movie.title
-#        print movie.videos
-#        movies[movie.title] = [movie.videos]
-#    except:
-#        attempt += 1
-#    print movies
-num = 500
-#Trailer info has to be called before calling .info()
-movie = tmdb.Movies(num)
-movies[num] = {'trailer':movie.videos()['results']}
+def six_random_movies():
+    attempt = 0 #safeguard against infinite loops
+    while (len(movies) < 6) and (attempt < 10):
+        num = random.randint(1,1000)
+        try:
+            #Trailer info has to be called before calling .info()
+            movie = tmdb.Movies(num)
+            movies[num] = {'trailer':movie.videos()['results']}
 
-processed_url = youtube+movies[num]['trailer'][0]['key']
-movies[num]['trailer'] = processed_url
-#add poster path
-movies[num]['poster'] = img_url+movie.info()['poster_path']
-#add movie title
-movies[num]['title'] = movie.info()['title']
-#add description
-movies[num]['description'] = movie.info()['overview']
-
+            processed_url = youtube+movies[num]['trailer'][0]['key']
+            movies[num]['trailer'] = processed_url
+            #add poster path
+            movies[num]['poster'] = img_url+movie.info()['poster_path']
+            #add movie title
+            movies[num]['title'] = movie.info()['title']
+            #add description
+            movies[num]['description'] = movie.info()['overview']
+        except:
+            attempt += 1
+while len(movies) < 6:
+    six_random_movies()
 print movies
-
-def vurl(video_obj):
-    print video_obj['key']
-#print movie.images()['backdrops']
-#movies[movie['id']] = {'Video':movie['video'],'Poster':movie['poster_path']}
