c9614bb7945f3e77a26604eb5ccdc2a2dff94c4d
Norsi <natalieannorsi@gmail.com>
2016-11-30 17:07:59 -0800

    documentation
    just a quick documentation line, accessible by __doc__()

--- Programming-Foundations/MiniProjects/movie-website/media.py
+++ Programming-Foundations/MiniProjects/movie-website/media.py
@@ -1,7 +1,8 @@
 import webbrowser
 class Movie():
+	"""This class provides a way to save movie information."""
 	VALID_RATINGS = ['G','R','PG','PG-13']
-	#Google Python Style Guide
+	#Refer to Google Python Style Guide
 	def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
 		self.title = title
 		self.storyline = storyline
