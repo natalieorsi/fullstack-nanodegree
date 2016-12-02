02a5e7ca4584fa966e455f335a34930b59c7551d
Natalie <natalieorsi@users.noreply.github.com>
2016-12-01 19:59:09 -0800

    final change, deleted old comments

--- Programming-Foundations/movie-website/entertainment_center.py
+++ Programming-Foundations/movie-website/entertainment_center.py
@@ -1,6 +1,5 @@
 import media, fresh_tomatoes, tmdb_api
 
-#movies = [toy_story,avatar,book_of_love,fences,eloise,fallen]
 movies_as_lsts = six_random_movies()
 
 #Format: Title, Description, Poster, Youtube
