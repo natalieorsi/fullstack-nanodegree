3e5efcd2c5089dee7b73fe5270f71a4e2dc5ac83
Norsi <natalieannorsi@gmail.com>
2016-12-01 16:01:33 -0800

    converted six_random_movies to list output
    still broken, though. but out of time for today

--- Programming-Foundations/movie-website/tmdb_api.py
+++ Programming-Foundations/movie-website/tmdb_api.py
@@ -13,28 +13,36 @@ import random
 youtube = 'http://www.youtube.com/watch?v='
 img_url = 'http://image.tmdb.org/t/p/w500'
 
-movies = {}
+
 def six_random_movies():
+    movies = []
     attempt = 0 #safeguard against infinite loops
-    while (len(movies) < 6) and (attempt < 10):
+    while (len(movies) < 2) and (attempt < 10):
         num = random.randint(1,1000)
+        new_movie = []
         try:
             #Trailer has to be called before calling .info()
             movie = tmdb.Movies(num)
             trailer = movie.videos()['results']
             movie = movie.info()
-            title = movie['title']
-            movies[title] = {}
-            #add trailer URL
-            processed_url = youtube+trailer['trailer'][0]['key']
-            movies[title]['trailer'] = processed_url
+            #add title
+            new_movie += [movie['title']]
+            #add youtube URL
+            processed_url = youtube+trailer['trailer'][0]['key']       
+            new_movie += [processed_url]
             #add poster path
-            movies[title]['poster'] = img_url+movie['poster_path']
+            new_movie += [img_url+movie['poster_path']]
             #add description
-            movies[title]['description'] = movie['overview']
+            new_movie += [movie['overview']]
+            print new_movie
         except:
             attempt += 1
-while len(movies) < 6:
-    six_random_movies()
-    
-print movies
+            print attempt
+        if len(new_movie) == 4:
+            movies += [new_movie]
+    return movies
+result = six_random_movies()
+#while result < 6:
+ #   six_random_movies()
+
+print result
