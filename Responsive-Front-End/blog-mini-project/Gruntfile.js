265abbb494393cb3f16776e7bebb1e5f5f42b53a
Norsi <natalieannorsi@gmail.com>
2016-12-04 16:36:40 -0800

    updated Gruntfile
    Decided not to run Grunt on this repo to avoid massive number of files
    uploading

--- Responsive-Front-End/blog-mini-project/Gruntfile.js
+++ Responsive-Front-End/blog-mini-project/Gruntfile.js
@@ -14,13 +14,9 @@ module.exports = function(grunt) {
         options: {
           engine: 'im',
           sizes: [{
-            /*
-            Change these:
-            
-            width: ,
-            suffix: ,
-            quality:
-            */
+            width: 1600,
+            suffix: '_large_2x'
+            quality: 30
           }]
         },
 
