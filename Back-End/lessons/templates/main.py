de4c03811503b8406d4660da63b88445844b0097
Norsi <natalieannorsi@gmail.com>
2016-12-12 16:49:53 -0800

    Playing with Jinja statements and variables

--- Back-End/lessons/templates/main.py
+++ Back-End/lessons/templates/main.py
@@ -17,16 +17,6 @@ import webapp2, os, jinja2
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
-form="""
-<title>Page Title</title>
-<form>
-<h2>Add a Food</h2>
-<input type="text" name="food">
-%s
-<button>Add</button>
-</form>
-"""
-
 item_html = "<li>%s</li>"
 
 hidden_html="""
@@ -44,33 +34,36 @@ shopping_list_html = """
 
 class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
-		self.response.out.write(*a,**kw)
+		self.response.out.write(*a, **kw)
 
 	def render_str(self, template, **params):
 		t = jinja_env.get_template(template)
 		return t.render(params)
 
 	def render(self, template, **kw):
-		self.write(self, render_str(template, **kw))
+		self.write(self.render_str(template, **kw))
 
 class MainPage(Handler):
 
 	def get(self):
-		output = form
-		output_hidden = ""
+		n = self.request.get("name")
+		self.render("shopping_list.html", name=n)
 
-		items = self.request.get_all("food")
-		output_items = ""
-		if items:
-			for item in items:
-				output_hidden += hidden_html % item
-				output_items += item_html % item
+		# output = form
+		# output_hidden = ""
 
-			output_shopping = shopping_list_html % output_items
-			output += output_shopping
+		# items = self.request.get_all("food")
+		# output_items = ""
+		# if items:
+		# 	for item in items:
+		# 		output_hidden += hidden_html % item
+		# 		output_items += item_html % item
 
-		output = output % output_hidden
-		self.write(output)
+		# 	output_shopping = shopping_list_html % output_items
+		# 	output += output_shopping
+
+		# output = output % output_hidden
+		# self.write(output)
 
 
 app = webapp2.WSGIApplication([('/', MainPage),
