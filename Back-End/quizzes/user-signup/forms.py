9dc6dcc79ff451e6fa79b4f94fb0dbfd691b070f
Norsi <natalieannorsi@gmail.com>
2016-12-13 17:14:15 -0800

    Start of user signup form mini-project

---
+++ Back-End/quizzes/user-signup/forms.py
@@ -0,0 +1,9 @@
+import cgi
+
+def valid_email(email):
+  if email:
+    if email contains '.' and len(email)>4:
+      return True
+  else:
+    return False
+
---
+++ Back-End/quizzes/user-signup/main.py
@@ -0,0 +1,37 @@
+import webapp2, os, jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+								autoescape = True)
+
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
+	title = "Sign up today!"
+
+	def get(self):
+		self.render("user_signup.html", title=title)
+
+	def post(self):
+		rot13 = ''
+		text = self.request.get('text')
+		if text:
+			rot13 = text.encode('rot13')
+
+		self.render("rot13.html", text=rot13, title=title)
+
+
+
+app = webapp2.WSGIApplication([('/', MainPage),
+							],
+							debug=True)
