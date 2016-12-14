d5369c532d857e957a53c91a651b9d0dc9f164a2
Norsi <natalieannorsi@gmail.com>
2016-12-13 18:07:58 -0800

    more elegant logic

--- Back-End/quizzes/user-signup/forms.py
+++ Back-End/quizzes/user-signup/forms.py
@@ -1,19 +1,13 @@
 import cgi, re
 
 def valid_email(email):
-  if email:
-      EM_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
-      return EM_RE.match(email)
-  return True
+  EM_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+  return not email or EM_RE.match(email)
 
 def valid_password(p):
-  if p:
-    PW_RE = re.compile(r"^.{3,20}$")
-    return PW_RE.match(p)
-  return False
+  PW_RE = re.compile(r"^.{3,20}$")
+  return p and PW_RE.match(p)
 
 def valid_username(username):
-  if username:
-    USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-    return USER_RE.match(username)
-  return False
+  USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+  return username and USER_RE.match(username)
